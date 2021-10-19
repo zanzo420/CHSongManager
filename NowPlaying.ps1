2372315c2da74ac3652266f1d557a5ea69a412cd
Zanzo420` <zp2kshield@gmail.com>
2021-10-19 04:29:17 -0700

    Create NowPlaying.ps1

---
+++ NowPlaying.ps1
@@ -0,0 +1,18 @@
+﻿$filePath = 'C:\Users\Zanzo\AppData\Roaming\Clone Hero Launcher\gameFiles'
+
+$fileSong = "$($filePath)\currentsong.txt"
+$fileSongs = "$($filePath)\songs.txt"
+$fileJSON = "$($filePath)\songs.json"
+
+$fileModded = "$($filePath)\nowplaying.txt"
+
+function NowPlaying()
+{
+    $tmpFile = Get-Content $fileSong
+    write-host $tmpFile -ForegroundColor Green
+    if($($tmpFile.Length -gt 0) -or $($($tmpFile) -ne $null))
+    {
+        write-host "[CHSM] currentsong.txt has data! Copying original to nowplaying.txt..."
+        Move-Item "$($filePath)\currentsong.txt" -Destination "$($filePath)\nowplaying.txt" -WhatIf
+    }
+}
