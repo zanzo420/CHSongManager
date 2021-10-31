c7291068f5fd2c34b74c6c58cda07888ee0eb0a0
Zanzo420` <zp2kshield@gmail.com>
2021-10-31 15:49:51 -0700

    misc

--- .garbage/ColoredTextFunctions.ps1
+++ .garbage/ColoredTextFunctions.ps1
@@ -15,3 +15,158 @@ function txtColor([string]$txt,[int]$color)
     
     #if($color -eq $null){write-host $txt -NoNewline} #null
 }
+
+function uiTextBar([string]$txt,[string]$type)
+{
+    if($($type -eq "Alert") -or $($type -eq "alert") -or $($type -eq "a") -or $($type -eq "A") -or $($type -eq "1"))
+    {
+        write-host "!|[ ALERT: " -NoNewline -ForegroundColor Yellow -BackgroundColor Black
+        write-host $($txt) -NoNewline -ForegroundColor DarkYellow -BackgroundColor Black
+        write-host " ]|!" -NoNewline -ForegroundColor Yellow -BackgroundColor Black   
+    } #Alert
+    elseif($($type -eq "Error") -or $($type -eq "error") -or $($type -eq "E") -or $($type -eq "e") -or $($type -eq "2"))
+    {
+        write-host "!|[ ERROR: " -NoNewline -ForegroundColor Red -BackgroundColor Black
+        write-host $($txt) -NoNewline -ForegroundColor DarkRed -BackgroundColor Black
+        write-host " ]|!" -NoNewline -ForegroundColor Red -BackgroundColor Black
+    } #Error
+    elseif($($type -eq "Info") -or $($type -eq "info") -or $($type -eq "I") -or $($type -eq "i") -or $($type -eq "3"))
+    {
+        write-host "!|[ INFO: " -NoNewline -ForegroundColor Cyan -BackgroundColor Black
+        write-host $($txt) -NoNewline -ForegroundColor DarkCyan -BackgroundColor Black
+        write-host " ]|!" -NoNewline -ForegroundColor Cyan -BackgroundColor Black
+    } #Info
+    elseif($($type -eq "Note") -or $($type -eq "note") -or $($type -eq "N") -or $($type -eq "n") -or $($type -eq "0"))
+    {
+        write-host "!|[ NOTE: " -NoNewline -ForegroundColor Gray -BackgroundColor Black
+        write-host $($txt) -NoNewline -ForegroundColor DarkGray -BackgroundColor Black
+        write-host " ]|!" -NoNewline -ForegroundColor Gray -BackgroundColor Black
+    } #Note
+    elseif($($type -eq "Red") -or $($type -eq "red") -or $($type -eq "R") -or $($type -eq "r") -or $($type -eq "4"))
+    {
+        write-host "!|[ " -NoNewline -ForegroundColor Red -BackgroundColor Black
+        write-host $($txt) -NoNewline -ForegroundColor DarkRed -BackgroundColor Black
+        write-host " ]|!" -NoNewline -ForegroundColor Red -BackgroundColor Black
+    } #Red
+    else 
+    {
+        write-host "!|[ " -ForegroundColor Red -BackgroundColor Black -NoNewline
+        write-host "$($type): " -NoNewline -ForegroundColor DarkRed -BackgroundColor Black
+        write-host $($txt) -NoNewline -BackgroundColor Black
+        write-host " ]|!" -NoNewline -ForegroundColor Red -BackgroundColor Black
+    } #Default
+
+}##END########################UI#uiTextError###
+
+function uiTextBlock([string]$txt,[int]$style)
+{
+    if($style -eq 1)
+    {
+        write-host "!|[ " -NoNewline -ForegroundColor Red -BackgroundColor Black
+        write-host $($txt) -NoNewline -BackgroundColor Black
+        write-host " ]|!" -NoNewline -ForegroundColor Red -BackgroundColor Black
+    } # Red/Default
+    elseif($style -eq 2)
+    {
+        write-host "!|[ " -ForegroundColor Red -BackgroundColor Black -NoNewline
+        write-host $($txt) -ForegroundColor DarkRed -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Red -BackgroundColor Black -NoNewline
+    } # Red/DarkRed
+    elseif($style -eq 3)
+    {
+        write-host "!|[ " -ForegroundColor Green -BackgroundColor Black -NoNewline
+        write-host $($txt) -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Green -BackgroundColor Black -NoNewline
+    } # Green/Default
+    elseif($style -eq 4)
+    {
+        write-host "!|[ " -ForegroundColor Green -BackgroundColor Black -NoNewline
+        write-host $($txt) -ForegroundColor DarkGreen -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Green -BackgroundColor Black -NoNewline
+    } # Green/DarkGreen
+    elseif($style -eq 5)
+    { 
+        write-host "!|[ " -ForegroundColor Blue -BackgroundColor Black -NoNewline
+        write-host $($txt) -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Blue -BackgroundColor Black -NoNewline
+    }# Blue/Default
+    elseif($style -eq 6)
+    { 
+        write-host "!|[ " -ForegroundColor Blue -BackgroundColor Black -NoNewline
+        write-host $($txt) -ForegroundColor DarkBlue -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Blue -BackgroundColor Black -NoNewline
+    }# Blue/DarkBlue
+    elseif($style -eq 7)
+    {
+        write-host "!|[ " -ForegroundColor Yellow -BackgroundColor Black -NoNewline
+        write-host $($txt) -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Yellow -BackgroundColor Black -NoNewline
+    } # Yellow/Default
+    elseif($style -eq 8)
+    { 
+        write-host "!|[ " -ForegroundColor Yellow -BackgroundColor Black -NoNewline
+        write-host $($txt) -ForegroundColor DarkYellow -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Yellow -BackgroundColor Black -NoNewline
+    }# Yellow/DarkYellow
+    elseif($style -eq 9)
+    { 
+        write-host "!|[ " -ForegroundColor Magenta -BackgroundColor Black -NoNewline
+        write-host $($txt) -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Magenta -BackgroundColor Black -NoNewline
+    }# Magenta/Default
+    elseif($style -eq 10)
+    {
+        write-host "!|[ " -ForegroundColor Magenta -BackgroundColor Black -NoNewline
+        write-host $($txt) -ForegroundColor DarkMagenta -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Magenta -BackgroundColor Black -NoNewline
+    } # Cyan/DarkMagenta
+    elseif($style -eq 11)
+    {
+        write-host "!|[ " -ForegroundColor Cyan -BackgroundColor Black -NoNewline
+        write-host $($txt) -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Cyan -BackgroundColor Black -NoNewline
+    } # Cyan/Default
+    elseif($style -eq 12)
+    {
+        write-host "!|[ " -ForegroundColor Cyan -BackgroundColor Black -NoNewline
+        write-host $($txt) -ForegroundColor DarkCyan -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Cyan -BackgroundColor Black -NoNewline
+    } # Cyan/DarkCyan
+    elseif($style -eq 13)
+    { 
+        write-host "!|[ " -ForegroundColor Gray -BackgroundColor Black -NoNewline
+        write-host $($txt) -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Gray -BackgroundColor Black -NoNewline
+    }# Gray/Default
+    elseif($style -eq 14)
+    {
+        write-host "!|[ " -ForegroundColor Gray -BackgroundColor Black -NoNewline
+        write-host $($txt) -ForegroundColor DarkGray -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor Gray -BackgroundColor Black -NoNewline
+    } # Gray/DarkGray
+    elseif($style -eq 15)
+    { 
+        write-host "!|[ " -ForegroundColor White -BackgroundColor Black -NoNewline
+        write-host $($txt) -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor White -BackgroundColor Black -NoNewline
+    }# White/Default
+    elseif($style -eq 16)
+    { 
+        write-host "!|[ " -ForegroundColor White -BackgroundColor Black -NoNewline
+        write-host $($txt) -ForegroundColor Gray -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor White -BackgroundColor Black -NoNewline
+    }# White/Gray
+    elseif($style -eq 0)
+    {
+        write-host "!|[ " -ForegroundColor Black -BackgroundColor White -NoNewline
+        write-host $($txt) -ForegroundColor Black -BackgroundColor White -NoNewline
+        write-host " ]|!" -ForegroundColor Black -BackgroundColor White -NoNewline
+    }# Black/White
+    else #DEFAULT
+    { 
+        write-host "!|[ " -ForegroundColor White -BackgroundColor Black -NoNewline
+        write-host $($txt) -ForegroundColor Gray -BackgroundColor Black -NoNewline
+        write-host " ]|!" -ForegroundColor White -BackgroundColor Black -NoNewline  
+    } # Red/Default
+
+}##END########################UI#TextBarRed###
--- CHSongManagerTools.ps1
+++ CHSongManagerTools.ps1
@@ -571,61 +571,8 @@ function dPause([string]$txt)
 {
     write-host "`n!|[ $($txt) ]|!" -ForegroundColor Yellow -BackgroundColor Black
     pause
-}
+}##END###########################dPause###
 
-################################################
-## NowPlaying FUNCTION ##############################
-<#function NowPlaying([string] $optText = "")
-{
-    ##VARIABLES######################
-    $curPath = 'C:\Users\Zanzo\AppData\Roaming\Clone Hero Launcher\gameFiles'
-    $curCurrentSong = "$($curPath)\currentsong.txt"
-    $curModded = "$($curPath)\nowplaying.txt"
-    ################################
-
-    # handle $optText if any
-    if($optText -ne ""){ write-host "The changed file was: $($optText)" -ForegroundColor Red -BackgroundColor Black }
-    write-host "Executing the 'NowPlaying' command on 'currentsong,txt' file..." -ForegroundColor Green -BackgroundColor Black
-
-    # execution of NowPlaying...
-    $curTempFile = Get-Content $curCurrentSong
-    write-host $curTempFile -ForegroundColor Green
-    if($($curTempFile.Length -gt 0) -or $($($curTempFile) -ne $null))
-    {
-        #make a copy of the "currentsong.txt" and save it to "nowplaying.txt"...
-        write-host "[CHSM] currentsong.txt has data! Copying original to nowplaying.txt..."
-        Copy-Item "$($curPath)\currentsong.txt" -Destination "$($curPath)\nowplaying.txt" -Force
-        #load the current song data from "nowplaying.txt"
-        $CurrentSong = Get-Content "$($curPath)\nowplaying.txt"
-        $curSong = $CurrentSong[0] #Song
-        $curArtist = $CurrentSong[1] #Artist
-        $curAlbum = $CurrentSong[2] #Album
-        $curYear = $CurrentSong[3] #Year
-        $curGenre = $CurrentSong[4] #Genre
-        $curPlaylist = $CurrentSong[5] #Playlist
-        $curCharter = $CurrentSong[6] #Charter
-        $outOneLine = "||[Song: $($CurrentSong[0]) || Artist: $($CurrentSong[1]) || Album: $($CurrentSong[2]) || Year: $($CurrentSong[3]) || Genre: $($CurrentSong[4]) || Playlist: $($CurrentSong[5]) || Charter: $($CurrentSong[6])]||"
-        $outALL = "Song: $($CurrentSong[0])`nArtist: $($CurrentSong[1])`nAlbum: $($CurrentSong[2])`nYear: $($CurrentSong[3])`nGenre: $($CurrentSong[4])`nPlaylist: $($CurrentSong[5])`nCharter: $($CurrentSong[6])"
-        $outNowPlaying = "$($curSong) by $($curArtist) (Charted by $($curCharter))"
-        write-host "!|[ NOW PLAYING ]|!" -ForegroundColor Green -BackgroundColor Black
-        write-host $($outNowPlaying)
-        #pause
-        write-host $outOneLine -ForegroundColor Magenta -BackgroundColor Black
-        #pause
-        write-host $outALL -ForegroundColor Cyan -BackgroundColor Black
-
-        #delete the nowplaying.txt file since it is no longer needed...
-        Remove-Item "$($curPath)\nowplaying.txt"
-    }else #if($curTempFile -eq "")
-    {
-        write-host "The 'currentsong.txt' file is empty." -ForegroundColor Red -BackgroundColor Black
-        return
-    }
-    write-host "Finished executing 'NowPlaying' command," #-ForegroundColor Green -BackgroundColor Black
-    Wait-FileChange -File $waitFile -Action $waitAction
-}##END###############################NowPlaying####
-################################################
-#>
 function RemoveSongDupes()
 {
     #search for this line to find start of duplicate songs...
@@ -633,7 +580,7 @@ function RemoveSongDupes()
 
     #regex for file to remove path...
     # [^)]*
-}
+}##END###################RemoveSongDupes###
 
 ################################################
 ## Wait-FileChange FUNCTION ###########################
